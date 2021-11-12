347ce4648fd36f66b2183dd5f054bc1e7d5425f5
JEROMEBRANCHETTI <jeromebranchetti00@gmail.com>
2021-11-12 18:03:46 +0100

    file saluto

---
+++ src/org/generation/italy/StampaSaluto.java
@@ -0,0 +1,10 @@
+package org.generation.italy;
+
+public class StampaSaluto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Questo è il messaggio di saluto!!");
+	}
+
+}
